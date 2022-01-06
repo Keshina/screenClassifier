cc65ea092086850706aa03207e240d9b5af47c52
Kesina <kesina@hotmail.com>
2022-01-06 12:54:20 -0500

    new image remaui results

---
+++ REMAUIOutputNew/signin_google_popup/otherapp-signin_google_popup-1/App-Skeleton/app/src/main/java/edu/wm/semeru/remaui_app/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.wm.semeru.remaui_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
